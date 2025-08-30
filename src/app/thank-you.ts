abd48119f29999a25127ce1aded8b56dd59699ca
Nigam Kumar Shu <nigamidea@gmail.com>
2025-08-30 20:55:06 +0530

    Update thank-you.ts

--- src/app/thank-you.ts
+++ src/app/thank-you.ts
@@ -6,8 +6,11 @@ import { RouterModule } from '@angular/router';
   standalone: true,
   imports: [RouterModule],
   template: `
-    <div class="p-4 max-w-lg mx-auto text-center">
-      <h1 class="text-2xl font-bold mb-4">🎉 Thanks for sharing your info!</h1>
+    <div
+      class="p-4 max-w-lg mx-auto text-center flex justify-center items-center flex-col h-screen"
+    >
+      <p class="text-5xl mb-4">🎉</p>
+      <h1 class="text-2xl font-bold mb-4">Thanks for sharing your info!</h1>
       <a routerLink="/" class="text-blue-500 underline">Go back to form</a>
     </div>
   `,
